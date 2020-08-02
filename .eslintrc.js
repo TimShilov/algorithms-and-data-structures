172287f9c6fa42f7cc1a5b52204bb2df0499708f
Tim Shilov <tim@shilov.dev>
2020-08-02 21:08:31 +0300

    chore: added some useful eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2020: true,
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:unicorn/recommended", "prettier"],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  plugins: ["unicorn"],
-  rules: {},
+    env: {
+        commonjs: true,
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "optimize-regex/recommended",
+        "plugin:node/recommended",
+        "plugin:promise/recommended",
+        "plugin:sonarjs/recommended",
+        "plugin:unicorn/recommended",
+        "prettier",
+    ],
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    plugins: ["unicorn", "optimize-regex", "promise", "sonarjs"],
+    rules: {},
 };
